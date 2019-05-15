e86e21f65f07df502753a3eaabe3b38923100522
Dejan <dejan.tomic@yahoo.com>
2019-05-15 22:50:56 +0200

    uključivanje vue2leaflet paketa i kreiranje mape

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="home">
-    <h1>Ovde se prikazuje mapa...samo se sada ne vidi (<i>kod mene radi !</i>)</h1>
-  </div>
+  <div id="map"></div>
 </template>
+
+<style>
+	#map {
+		height: 500px;
+	}
+</style>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default {
+	data() {
+		return {
+			map: null,
+			tileLayer: null,
+			layers: []
+		}
+	},
+	mounted() {
+		this.initMap(),
+		this.initLayers()
+	},
+	methods: {
+		initMap() {
+			this.map = L.map('map').setView([44.7866, 20.4489], 15)
+			/* https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png */
+			this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(this.map);
+		},
+		initLayers() {}
+	},
+	components: {
+		LMap,
+		LTileLayer,
+		LMarker
+	}
+}
+</script>
