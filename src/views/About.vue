e7e6382325dd3ed151052156e15bd720bfff7f5a
Dejan <dejan.tomic@yahoo.com>
2019-05-16 18:52:05 +0200

    bootstrap vue paket

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-6">
+  				<h1>This is an about page</h1>
+  			</div>
+  			<div class="col-md-6">
+  				<h1>Ovo je druga kolona</h1>
+  			</div>	
+  		</div>
+  	</div>
+    
   </div>
 </template>
