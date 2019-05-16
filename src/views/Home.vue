710e158a82a46d81917940ae5710a15744ed5d2e
Dejan <dejan.tomic@yahoo.com>
2019-05-16 20:35:00 +0200

    get current user location

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="map"></div>
+  <div>
+    <div class="loc"><button @click="showUserLoc" class="loc btn btn-primary">Prikaži moju lokaciju</button></div>
+    <p>User Latitude: {{ userLat }}</p>
+    <p>User Longitude: {{ userLong }}</p>
+	<div id="map"></div>
+  </div>
 </template>
 
 <style>
 	#map {
 		height: 500px;
+	}
+
+	.loc {
+		margin-bottom: 15px;
 	}
 </style>
 
@@ -17,7 +26,10 @@ export default {
 		return {
 			map: null,
 			tileLayer: null,
-			layers: []
+			layers: [],
+			error: '',
+			userLat: '',
+			userLong: ''
 		}
 	},
 	mounted() {
@@ -29,10 +41,21 @@ export default {
 			this.map = L.map('map').setView([44.7866, 20.4489], 15)
 			/* https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png */
 			this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(this.map);
+    		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(this.map)
 		},
-		initLayers() {}
+		initLayers() {},
+		showUserLoc() {
+			if (navigator.geolocation) {
+			    navigator.geolocation.getCurrentPosition(this.showPosition);
+			  } else {
+			    this.error = "Geolocation is not supported by this browser.";
+			  }
+		},
+		showPosition(position) {
+			this.userLat =  position.coords.latitude
+			this.userLong = position.coords.longitude
+		}
 	},
 	components: {
 		LMap,
