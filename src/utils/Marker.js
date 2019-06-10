8c2cc29eee775dc4e865c5fecb3c264a22bdbd99
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-10 11:31:53 +0200

    add todo comment

--- src/utils/Marker.js
+++ src/utils/Marker.js
@@ -5,11 +5,10 @@ import { Icon, latLng } from 'leaflet'
   https://github.com/pointhi/leaflet-color-markers
 */
 const makeIcon = color => new Icon({
+  // TODO: implement category like this: iconUrl: require(`../assets/images/${item.kategorija}.svg`),
   iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
   shadowSize: [41, 41]
 })
 
