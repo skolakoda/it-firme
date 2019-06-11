4eae2fe0f08d586e92a7ac46b5bf6a8abdff7d73
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-11 13:34:47 +0200

    fix linter

--- src/views/Mapa.vue
+++ src/views/Mapa.vue
@@ -69,26 +69,26 @@
 </style>
 
 <script>
-import { Icon, latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
-import "leaflet/dist/leaflet.css";
+import { Icon, latLng } from 'leaflet'
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
 
-import Marker, { colors } from "../utils/Marker";
+import Marker, { colors } from '../utils/Marker'
 
 export default {
-  data() {
+  data () {
     return {
       pokazatiPoziciju: false,
       zoom: 12,
       center: latLng(44.7866, 20.4489),
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       markers: [],
       allLocations: [],
       categories: [],
       izabrano: []
-    };
+    }
   },
   components: {
     LMap,
@@ -97,46 +97,46 @@ export default {
     LPopup,
     LTooltip
   },
-  mounted() {
+  mounted () {
     // fix default icons
-    delete Icon.Default.prototype._getIconUrl;
+    delete Icon.Default.prototype._getIconUrl
     Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-    });
-    this.getLocations();
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    })
+    this.getLocations()
   },
   computed: {
-    filteredItems() {
-      return this.markers.filter(item => this.izabrano.includes(item.category));
+    filteredItems () {
+      return this.markers.filter(item => this.izabrano.includes(item.category))
     }
   },
   methods: {
-    getLocations() {
-      fetch("https://spomenici-api.herokuapp.com/kolekcija/itfirme")
+    getLocations () {
+      fetch('https://spomenici-api.herokuapp.com/kolekcija/itfirme')
         .then(res => res.json())
         .then(res => {
-          this.allLocations = res.data;
+          this.allLocations = res.data
           this.izabrano = this.categories = [
             ...new Set(this.allLocations.map(item => item.kategorija))
-          ];
+          ]
           this.markers = this.allLocations.map(item => {
-            const catIndex = this.categories.indexOf(item.kategorija);
-            return new Marker(item, this.getColor(catIndex));
-          });
-        });
+            const catIndex = this.categories.indexOf(item.kategorija)
+            return new Marker(item, this.getColor(catIndex))
+          })
+        })
     },
-    showUserLoc() {
+    showUserLoc () {
       navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        this.center = latLng(latitude, longitude);
-        this.pokazatiPoziciju = true;
-      });
+        const { latitude, longitude } = position.coords
+        this.center = latLng(latitude, longitude)
+        this.pokazatiPoziciju = true
+      })
     },
-    getColor(i) {
-      return colors[i % colors.length];
+    getColor (i) {
+      return colors[i % colors.length]
     }
   }
-};
+}
 </script>
