ae734e879f78a8ef8c57215a36633dca08b59e44
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-12 12:24:28 +0200

    add show/hide

--- src/views/Prijava.vue
+++ src/views/Prijava.vue
@@ -1,87 +1,108 @@
 <template>
-  <div class="log">
+  <div class="prijava">
+
     <div class="row">
       <div class="col-sm-6">
         <button
           type="button"
           class="btn btn-outline-success btn-sm btn-block"
-          v-bind:class="[isActive ? 'active':'']"
-          @click="toggleCalss"
-        >Log Up</button>
+          v-bind:class="{active : showRegister}"
+          @click="toggleView"
+        >Sign Up</button>
       </div>
       <div class="col-sm-6">
         <button
           type="button"
           class="btn btn-outline-success btn-sm btn-block"
-          v-bind:class="[isActive ? '':'active']"
-          @click="isActive=!isActive"
+          v-bind:class="{active : !showRegister}"
+          @click="toggleView"
         >Log In</button>
       </div>
     </div>
-    <br>
 
-    <form>
-      <div class="form-group" v-bind:class="[isActive ? '':'test']">
-        <h4>Log Up</h4>
-        <hr>
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        >
-        <small id="emailHelp" class="form-text text-muted"></small>
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        >
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
-      <div class="form-group" v-bind:class="[isActive ? 'test':'']">
-        <h4>Log In</h4>
-        <hr>
-        <label for="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Username or Enter email"
-        >
-        <small id="emailHelp" class="form-text text-muted"></small>
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        >
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
+    <form v-if="showRegister">
+      <h4>Sign Up</h4>
+      <hr>
+      <label for="registerEmail">Email</label>
+      <input
+        name="email"
+        type="email"
+        class="form-control"
+        id="registerEmail"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      >
+      <small id="emailHelp" class="form-text text-muted"></small>
+      <label for="registerPassword1">Password</label>
+      <input
+        name="password"
+        type="password"
+        class="form-control"
+        id="registerPassword1"
+        placeholder="Enter password"
+      >
+      <label for="registerPassword2">Repeat password</label>
+      <input
+        name="repeatPassword"
+        type="password"
+        class="form-control"
+        id="registerPassword2"
+        placeholder="Enter password"
+      >
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <form v-if="!showRegister">
+      <h4>Log In</h4>
+      <hr>
+      <label for="loginEmail">Email</label>
+      <input
+        name="email"
+        type="email"
+        class="form-control"
+        id="loginEmail"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      >
+      <small id="emailHelp" class="form-text text-muted"></small>
+      <label for="loginPassword">Password</label>
+      <input
+        name="password"
+        type="password"
+        class="form-control"
+        id="loginPassword"
+        placeholder="Enter password"
+      >
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
 
 <style>
-.log {
+.prijava {
   width: 30rem;
   margin: 1rem auto;
   border: 1px solid #42b983;
   border-radius: 5px;
   padding: 1rem;
 }
+
+form {
+  margin-top: 15px;
+}
 </style>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      showRegister: true
+    }
   },
-  methods: {}
+  methods: {
+    toggleView () {
+      this.showRegister = !this.showRegister
+    }
+  }
 }
 </script>
